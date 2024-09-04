c4bca50363fb7fd45d7fd6097fcabd3bfffdde28
gus ryan <rian.putra2003@gmail.com>
2024-09-04 09:55:51 +0800

    update slider and slider-card in homepage

--- components/homepage/Slider.vue
+++ components/homepage/Slider.vue
@@ -21,43 +21,19 @@
                 }" :modules="modules" :breakpoints="breakpoints" :space-between="24" navigation
                 :pagination="{ clickable: true }" class="h-full w-full">
                 <swiper-slide class="relative">
-                    <img src="/public/dining-browse-image.png" alt="" class="w-full h-full object-cover">
-                    <div class="absolute w-full flex flex-col-reverse xl:flex-row xl:items-end p-6 lg:p-4 bottom-0">
-                        <div class="relative w-fit lg:min-w-56 h-32">
-                            <div class="w-full h-full flex flex-col justify-center p-3 lg:p-6 z-10">
-                                <p class="text-[#616161] font-bold z-10 flex items-center gap-2">
-                                    <span>
-                                        01
-                                    </span>
-                                <div class="w-8 h-[2px] bg-[#616161]"></div>
-                                <span>
-                                    Bed Room
-                                </span>
-                                </p>
-                                <h2 class="text-3xl font-bold z-10">
-                                    Inner Peace
-                                </h2>
-                            </div>
-                            <div class="absolute top-0 bg-white opacity-75 w-full h-full z-0"></div>
-                        </div>
-                        <div class="w-[50%]">
-                            <ButtonPrimary class="px-0 lg:w-[80%]" :color="'bg-primary'">
-                                <ArrowRightIcon class="size-9 min-w-8 transition-all group-hover:translate-x-4" />
-                            </ButtonPrimary>
-                        </div>
-                    </div>
+                    <HomepageSliderCard />
                 </swiper-slide>
-                <swiper-slide>
-                    <img src="/public/dining-browse-image.png" alt="" class="w-full h-full max-h-[480px] object-cover">
+                <swiper-slide class="relative">
+                    <HomepageSliderCard />
                 </swiper-slide>
-                <swiper-slide>
-                    <img src="/public/dining-browse-image.png" alt="" class="w-full h-full max-h-[480px] object-cover">
+                <swiper-slide class="relative">
+                    <HomepageSliderCard />
                 </swiper-slide>
-                <swiper-slide>
-                    <img src="/public/dining-browse-image.png" alt="" class="w-full h-full max-h-[480px] object-cover">
+                <swiper-slide class="relative">
+                    <HomepageSliderCard />
                 </swiper-slide>
-                <swiper-slide>
-                    <img src="/public/dining-browse-image.png" alt="" class="w-full h-full max-h-[480px] object-cover">
+                <swiper-slide class="relative">
+                    <HomepageSliderCard />
                 </swiper-slide>
             </swiper>
         </div>
