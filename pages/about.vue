6407861578633ae882bc3538c8b38f8314f07fef
gus ryan <rian.putra2003@gmail.com>
2024-09-06 20:24:09 +0800

    set up about page view

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <Main>
+        <section class="mt-24">
+            <HeroImage>
+                <template #header>
+                    About Furnicons E Commerce 
+                </template>
+                <template #detail>
+                    About
+                </template>
+            </HeroImage>
+        </section>
+        <section>
+            
+        </section>
+    </Main>
+</template>
+
+<script setup lang="ts">
+import Main from '~/layouts/main.vue';
+
+
+</script>
