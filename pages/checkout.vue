3892cf0f1922b6aae487fc7aaa979bf09e795fe5
gus ryan <rian.putra2003@gmail.com>
2024-09-08 11:59:08 +0800

    set up checkout page after cart

---
+++ pages/checkout.vue
@@ -0,0 +1,23 @@
+<template>
+    <Main>
+        <section class="mt-24">
+            <HeroImage>
+                <template #header>
+                    Checkout
+                </template>
+                <template #detail>
+                    Checkout
+                </template>
+            </HeroImage>
+        </section>
+        <section>
+            
+        </section>
+    </Main>
+</template>
+
+<script setup lang="ts">
+import Main from '~/layouts/main.vue';
+
+
+</script>
