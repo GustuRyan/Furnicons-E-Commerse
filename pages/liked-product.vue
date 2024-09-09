275c2f40dbd04cd90c2c04076b98d1206cb195a1
gus ryan <rian.putra2003@gmail.com>
2024-09-09 15:25:33 +0800

    set up for liked products page

---
+++ pages/liked-product.vue
@@ -0,0 +1,23 @@
+<template>
+    <Main>
+        <section class="mt-24">
+            <HeroImage>
+                <template #header>
+                    Liked Product 
+                </template>
+                <template #detail>
+                    Liked
+                </template>
+            </HeroImage>
+        </section>
+        <section>
+            
+        </section>
+    </Main>
+</template>
+
+<script setup lang="ts">
+import Main from '~/layouts/main.vue';
+
+
+</script>
