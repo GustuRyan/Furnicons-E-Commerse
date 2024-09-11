edd18da6db0e867dca061f6c60618d335cd8fac4
gus ryan <rian.putra2003@gmail.com>
2024-09-11 17:53:21 +0800

    set up blog page in the website

---
+++ pages/blog.vue
@@ -0,0 +1,23 @@
+<template>
+    <Main>
+        <section class="mt-24">
+            <HeroImage>
+                <template #header>
+                    Our Blog 
+                </template>
+                <template #detail>
+                    Blog
+                </template>
+            </HeroImage>
+        </section>
+        <section>
+            
+        </section>
+    </Main>
+</template>
+
+<script setup lang="ts">
+import Main from '~/layouts/main.vue';
+
+
+</script>
