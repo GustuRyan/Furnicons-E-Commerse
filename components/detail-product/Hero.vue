30ce2a5bfc34ac6f7249391ace13e95c057a7946
gus ryan <rian.putra2003@gmail.com>
2024-09-16 21:18:48 +0800

    add and setup hero in product detail page

---
+++ components/detail-product/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="h-[100px] bg-secondary px-8 md:px-16 lg:px-20 flex items-center gap-3">
+            <h3>
+                Home
+            </h3>
+            <ChevronRightIcon class="size-4"/>
+            <h3>
+                Shop
+            </h3>
+            <ChevronRightIcon class="size-4"/>
+            <div class="h-9 w-[1.5px] rounded-full bg-[#9F9F9F]"></div>
+            <h3>
+                Asgard Sofa
+            </h3>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ChevronRightIcon } from '@heroicons/vue/24/outline';
+
+
+</script>
