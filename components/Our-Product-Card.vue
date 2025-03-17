454d55ca8276c242cd53f637e2a1c2ed93b383ec
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-03-17 23:16:19 +0800

    update Our Product Card

--- components/Our-Product-Card.vue
+++ components/Our-Product-Card.vue
@@ -16,7 +16,7 @@
                 </span>
                 <div class="flex justify-between items-center">
                     <span class="text-xl font-bold">
-                        Rp 2.000.000
+                        Rp 2.500.000
                     </span>
                     <span class="text-sm text-[#898989] line-through">
                         Rp 3.000.000
