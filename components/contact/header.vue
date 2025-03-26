477f1977e899695a316f590b76091c0881663868
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-03-26 20:46:13 +0800

    update header

--- components/contact/header.vue
+++ components/contact/header.vue
@@ -5,7 +5,7 @@
         </h1>
         <p class="text-lg text-[#9F9F9F] lg:w-[640px]">
             For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be
-            There To Help You Out. Do Not Hesitate!
+            There To Help You Out. Do Not Hesitate!!
         </p>
     </div>
 </template>
