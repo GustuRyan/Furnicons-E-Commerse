2ae4883e53367b29a33abe4524e52e25773efd62
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-03-31 23:22:56 +0800

    add the logos in product detail hero

--- components/detail-product/Hero.vue
+++ components/detail-product/Hero.vue
@@ -98,9 +98,9 @@
                         <p>Sofas</p>
                         <p>Sofa, Chair, Home, Shop</p>
                         <div class="flex gap-3">
-                            <img src="" alt="" class="w-2 h-2">
-                            <img src="" alt="" class="w-2 h-2">
-                            <img src="" alt="" class="w-2 h-2">
+                            <img src="/public/facebook_logo.svg" alt="" class="w-2 h-2">
+                            <img src="/public/linkedin_logo.svg" alt="" class="w-2 h-2">
+                            <img src="/public/X_logo.svg" alt="" class="w-2 h-2">
                         </div>
                     </div>
                 </div>
